3cde6e1c6d01912c41c94e717b754a730429b101
mariadmos <dalva.maria.mos@gmail.com>
2025-06-09 08:50:23 -0300

    Adicionado suporte a strings e numeros

--- src/main/java/br/com/ufma/compiladores/Scanner.java
+++ src/main/java/br/com/ufma/compiladores/Scanner.java
@@ -61,8 +61,49 @@ class Scanner {
         case '\n':
             line++;
             break;
-        default: Lox.error(line, "Unexpected character."); break;
+        case '"': string(); break;
+        default: 
+            if (isDigit(c)) {
+                number();
+            } else {
+                Lox.error(line, "Unexpected character.");
+            }
+            break;
         }
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+    
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+          // Consume the "."
+          advance();
+    
+          while (isDigit(peek())) advance();
+        }
+    
+        addToken(NUMBER,
+            Double.parseDouble(source.substring(start, current)));
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+          if (peek() == '\n') line++;
+          advance();
+        }
+    
+        if (isAtEnd()) {
+          Lox.error(line, "Unterminated string.");
+          return;
+        }
+    
+        // The closing ".
+        advance();
+    
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
@@ -77,6 +118,15 @@ class Scanner {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    } 
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    } 
 
     private boolean isAtEnd() {
         return current >= source.length();
