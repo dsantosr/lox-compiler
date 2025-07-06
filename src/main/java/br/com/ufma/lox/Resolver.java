c01048f9f03cb139f7eb355ca8532f07f7eee2c3
mariadmos <dalva.maria.mos@gmail.com>
2025-07-06 00:01:58 -0300

    Adicionando construtores com init()

--- src/main/java/br/com/ufma/lox/Resolver.java
+++ src/main/java/br/com/ufma/lox/Resolver.java
@@ -17,6 +17,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
         NONE,
         FUNCTION,
+        INITIALIZER,
         METHOD
     }
 
@@ -47,6 +48,9 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
         for (Stmt.Function method : stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
+            if (method.name.lexeme().equals("init")) {
+                declaration = FunctionType.INITIALIZER;
+            }
             resolveFunction(method, declaration);
         }
 
@@ -93,6 +97,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         if (stmt.value != null) {
+            if (currentFunction == FunctionType.INITIALIZER) {
+                Lox.error(stmt.keyword.line(),
+                        "Can't return a value from an initializer.");
+            }
             resolve(stmt.value);
         }
 
