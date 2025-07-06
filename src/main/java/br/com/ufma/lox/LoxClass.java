f198df83d2323d0dc57bfb019901bb3f19766d68
mariadmos <dalva.maria.mos@gmail.com>
2025-07-05 23:41:52 -0300

    Adicionando suporte a metodos para as classes

--- src/main/java/br/com/ufma/lox/LoxClass.java
+++ src/main/java/br/com/ufma/lox/LoxClass.java
@@ -3,27 +3,37 @@ package br.com.ufma.lox;
 import java.util.List;
 import java.util.Map;
 
-class LoxClass implements LoxCallable{
-  final String name;
+class LoxClass implements LoxCallable {
+    final String name;
+    private final Map<String, LoxFunction> methods;
 
-  LoxClass(String name) {
-    this.name = name;
-  }
+    LoxClass(String name, Map<String, LoxFunction> methods) {
+        this.name = name;
+        this.methods = methods;
+    }
 
-@Override
-  public Object call(Interpreter interpreter,
-                     List<Object> arguments) {
-    LoxInstance instance = new LoxInstance(this);
-    return instance;
-  }
+    LoxFunction findMethod(String name) {
+        if (methods.containsKey(name)) {
+            return methods.get(name);
+        }
 
-  @Override
-  public int arity() {
-    return 0;
-  }
-  
-  @Override
-  public String toString() {
-    return name;
-  }
+        return null;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter,
+            List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
